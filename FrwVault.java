71d1061156e85aa1e16130cf60e2836e850c6292
MinJun Jin <47666384+minjun-jin@users.noreply.github.com>
2026-01-27 14:06:48 +0900

    Update FrwVault.java

--- FrwVault.java
+++ FrwVault.java
@@ -3,13 +3,10 @@ package com.jpmc.kcg.frw;
 import java.security.GeneralSecurityException;
 import java.security.Provider;
 import java.security.Security;
-import java.util.Arrays;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.TreeMap;
 
 import javax.crypto.Cipher;
-import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import javax.management.openmbean.InvalidKeyException;
 
@@ -33,7 +30,7 @@ import lombok.extern.slf4j.Slf4j;
 public class FrwVault implements InitializingBean {
 
 	private final SystemProperties systemProperties;
-	private Map<String, Entry<byte[], byte[]>> vKeyMap = new TreeMap<>();
+	private Map<String, byte[]> vKeyMap = new TreeMap<>();
 	private ThreadLocal<Map<String, Cipher>> threadLocal = ThreadLocal.withInitial(TreeMap::new);
 	private Cipher newCipher(int opmode, String key) throws GeneralSecurityException {
 ////////////////////////////////////////////////////////////////////////////////
@@ -50,22 +47,22 @@ public class FrwVault implements InitializingBean {
 		Map<String, Cipher> map = threadLocal.get();
 		Cipher cipher = map.get(str);
 		if (null == cipher) {
-			Entry<byte[], byte[]> entry = vKeyMap.get(key);
-			if (null == entry) {
+			byte[] byteArray = vKeyMap.get(key);
+			if (null == byteArray) {
 				throw new InvalidKeyException(key);
 			}
-			String transformation = "AES/CBC/PKCS5Padding";
+			String x = "AES/ECB/PKCS7Padding";
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 /////////// SEED ///////////////////////////////////////////////////////////////
-			if (32 > entry.getKey().length) {
-				transformation = "SEED/CBC/PKCS5Padding";
+			if (32 > byteArray.length) {
+				x = "SEED/ECB/ISO7816-4Padding";
 			}
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
-			cipher = Cipher.getInstance(transformation);
-			cipher.init(opmode, new SecretKeySpec(entry.getKey(), "AES"), new IvParameterSpec(entry.getValue()));
+			cipher = Cipher.getInstance(x);
+			cipher.init(opmode, new SecretKeySpec(byteArray, StringUtils.substringBefore(x, '/')));
 			map.put(str, cipher);
 		}
 		return cipher;
@@ -79,22 +76,15 @@ public class FrwVault implements InitializingBean {
 		log.debug("afterPropertiesSet");
 		systemProperties.getVaultKey().entrySet().forEach(entry -> {
 			log.debug("{}", entry);
-			byte[] key;
-			byte[] iv;
+			byte[] byteArray = null;
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 /////////// 볼트키암호화키복호화 ///////////////////////////////////////////////
-			String[] stringArray = StringUtils.split(entry.getValue(), '&');
-			key = Base64.decodeBase64(stringArray[0]);
-			if (1 < stringArray.length) {
-				iv = Base64.decodeBase64(stringArray[1]);;
-			} else {
-				iv = Arrays.copyOf(key, 16);
-			}
+			byteArray = Base64.decodeBase64(entry.getValue());
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
-			vKeyMap.put(entry.getKey(), Map.entry(key, iv));
+			vKeyMap.put(entry.getKey(), byteArray);
 		});
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
@@ -107,6 +97,7 @@ public class FrwVault implements InitializingBean {
 			enc = Base64.encodeBase64String(newCipher(Cipher.ENCRYPT_MODE, key).doFinal(StringUtils.getBytes(str, "EUC-KR")));
 		} catch (Throwable t) {
 			log.error("encB64Str {}, {}", key, str);
+			log.error("encB64Str", t);
 		}
 		return enc;
 	}
@@ -117,6 +108,7 @@ public class FrwVault implements InitializingBean {
 			dec = IOUtils.toString(newCipher(Cipher.DECRYPT_MODE, key).doFinal(Base64.decodeBase64(str)), "EUC-KR");
 		} catch (Throwable t) {
 			log.error("decB64Str {}, {}", key, str);
+			log.error("decB64Str", t);
 		}
 		return dec;
 	}
@@ -127,6 +119,7 @@ public class FrwVault implements InitializingBean {
 			enc = Hex.encodeHexString(newCipher(Cipher.ENCRYPT_MODE, key).doFinal(StringUtils.getBytes(str, "EUC-KR")));
 		} catch (Throwable t) {
 			log.error("encHexStr {}, {}", key, str);
+			log.error("encHexStr", t);
 		}
 		return enc;
 	}
@@ -137,6 +130,7 @@ public class FrwVault implements InitializingBean {
 			dec = IOUtils.toString(newCipher(Cipher.DECRYPT_MODE, key).doFinal(Hex.decodeHex(str)), "EUC-KR");
 		} catch (Throwable t) {
 			log.error("decHexStr {}, {}", key, str);
+			log.error("decHexStr", t);
 		}
 		return dec;
 	}
